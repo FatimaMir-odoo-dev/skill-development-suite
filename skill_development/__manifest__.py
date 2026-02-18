c0338cf37e9b0e73e8b5d7bfe15e5783f4ddf2dd
Fatima Mirghani <fatimamirghani24@gmail.com>
2026-02-18 03:48:43 -0800

    Change the module version in manifest to the latest 16.0.3.0

--- skill_development/__manifest__.py
+++ skill_development/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=pointless-statement,missing-module-docstring
 {
     'name': 'Skill Development',
-    'version': '16.0.2.33',
+    'version': '16.0.3.0',
     'summary': 'Personalized learning plans for skill development.',
     'description': """
         It's the core of the Skill Development Suite.
