068359b415f2fadd4eb8a7340c9b28c04229b1d1
Fatima Mirghani <fatimamirghani24@gmail.com>
2026-02-27 08:46:58 -0800

    Clean models: CreateInitialPlan

--- skill_development/wizard/create_initial_plan.py
+++ skill_development/wizard/create_initial_plan.py
@@ -13,7 +13,6 @@ class CreateInitialPlan(models.TransientModel):
     _name = 'skill_development.create_initial_plan'
     _description = 'Skill Planning Pop-up Form'
 
-# learner_id used in default get form to get the current user id
     learner_id = fields.Many2one(
         'res.users',
         string='Learner',
@@ -27,19 +26,6 @@ class CreateInitialPlan(models.TransientModel):
     motivation = fields.Text('My Motivation', required=True)
     endpoint = fields.Date('Preferred Endpoint')
     msg_2self = fields.Text('Message to Myself')
-
-# get learner id and skill name from context passed to the wizard form
-#     @api.model
-#     def default_get(self, fields_list):
-#         res = super(SkillPlanWizard, self).default_get(fields_list)
-#         learner_id = self.env.context.get('default_learner_id')
-#         skill_id = self.env.context.get('default_skill_id')
-#
-#         if learner_id:
-#             res['learner_id'] = learner_id  # Set the default value for learner_id
-#         if skill_id:
-#             res['skill_id'] = skill_id  # Set the default value for skill_name
-#         return res
 
     def button_create_plan(self):
         """
